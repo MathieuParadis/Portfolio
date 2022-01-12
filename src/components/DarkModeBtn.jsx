c8d1df79ad3c4be77d9c7f6518459bc1d6a3cf9f
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-12 12:43:26 +0100

    finished editing burger menu

--- src/components/DarkModeBtn.jsx
+++ src/components/DarkModeBtn.jsx
@@ -12,7 +12,7 @@ const DarkModeBtn = () => {
   const { darkMode, setDarkMode } = useContext(DarkMode);
 
   return (
-    <div className="d-flex align-items-center">
+    <div className="dark-mode-btn d-flex align-items-center">
       <input type="checkbox" className="" id="dark-mode-checkbox" checked={darkMode ? true : false} onChange={setDarkMode}/>
       <label htmlFor="dark-mode-checkbox"><img className="darkmode-logo" src={darkMode ? sun : moon} alt="Dark mode toggler button" /></label>
     </div>
--- src/components/DarkModeBtnMobile.jsx
+++ src/components/DarkModeBtnMobile.jsx
@@ -12,7 +12,7 @@ const DarkModeBtnMobile = () => {
   const { darkMode, setDarkMode } = useContext(DarkMode);
 
   return (
-    <div className="d-flex align-items-center">
+    <div className="dark-mode-btn-mobile d-flex align-items-center">
       <input type="checkbox" className="" id="dark-mode-checkbox" checked={darkMode ? true : false} onChange={setDarkMode}/>
       <label htmlFor="dark-mode-checkbox"><img className="darkmode-logo" src={darkMode ? sun : moon} alt="Dark mode toggler button" /></label>
     </div>
