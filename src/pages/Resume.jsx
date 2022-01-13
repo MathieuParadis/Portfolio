22fb06fe676a42bf1cb666e1d2daceb82801a81e
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-13 13:14:45 +0100

    created coding language section component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,22 @@
 // CONFIG IMPORTS
 import React from 'react';
 
+// COMPONENTS IMPORTS
+import CodingLanguageSection from '../components/CodingLanguageSection';
+
 const Resume = () => {
   return (
     <div className="resume">
       <div className="page">
-        This is the resume page
+        <div className="content">
+          <div className="head">
+            <h1><strong>Mathieu Paradis</strong></h1>
+            <p>French Web Developer based between Europe and Asia. Passionate about minimalism, sustainability, coffee and bread.</p>
+          </div>
+          <div className="body">
+            <CodingLanguageSection />
+          </div>
+        </div>
       </div>
     </div>
   );
