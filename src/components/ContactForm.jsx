8dfdcdea30616181bc13162f78b095c86958e854
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-18 10:01:11 +0100

    edited form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -18,10 +18,26 @@ const ContactForm = () => {
     let subjectInput = document.getElementById("subject");
     let messageInput = document.getElementById("message");
     let inputs = [nameInput, emailInput, subjectInput, messageInput];
-    let formMessage = document.querySelector(".formMessage");
+    let formNotification = document.querySelector(".form-notification");
+
+    const isNameFilled = () => {
+      let NameErrorLabel = document.getElementById("name-label");
+
+      if (name) {
+        NameErrorLabel.style.display = "none";
+        return true;
+      } else {
+        NameErrorLabel.style.display = "block";
+        NameErrorLabel.style.animation = "dongle 1s";
+        setTimeout(() => {
+          NameErrorLabel.style.animation = "none";
+        }, 1000);
+        return false;
+      }
+    };
 
     const isEmailCorrect = () => {
-      let MailErrorLabel = document.getElementById("not-mail");
+      let MailErrorLabel = document.getElementById("mail-label");
       let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
       if (email.match(regex)) {
@@ -37,8 +53,40 @@ const ContactForm = () => {
       }
     };
 
+    const isSubjectFilled = () => {
+      let SubjectErrorLabel = document.getElementById("subject-label");
+
+      if (subject) {
+        SubjectErrorLabel.style.display = "none";
+        return true;
+      } else {
+        SubjectErrorLabel.style.display = "block";
+        SubjectErrorLabel.style.animation = "dongle 1s";
+        setTimeout(() => {
+          SubjectErrorLabel.style.animation = "none";
+        }, 1000);
+        return false;
+      }
+    };
+
+    const isMessageFilled = () => {
+      let MessageErrorLabel = document.getElementById("message-label");
+
+      if (message) {
+        MessageErrorLabel.style.display = "none";
+        return true;
+      } else {
+        MessageErrorLabel.style.display = "block";
+        MessageErrorLabel.style.animation = "dongle 1s";
+        setTimeout(() => {
+          MessageErrorLabel.style.animation = "none";
+        }, 1000);
+        return false;
+      }
+    };
+    
     const areFieldsfilled = () => {
-      if (name && isEmailCorrect() && subject && message) {
+      if (isNameFilled() && isEmailCorrect() && isSubjectFilled() && isMessageFilled()) {
         return true
       } else {
         return false
@@ -51,39 +99,36 @@ const ContactForm = () => {
       subjectInput.classList.remove("red");
       messageInput.classList.remove("red");
 
-      formMessage.innerHTML = "Message en cours d'envoi...";
-      formMessage.style.background = "#00c1ec";
-      formMessage.style.opacity = "1";
+      formNotification.innerText = "Message en cours d'envoi...";
+      formNotification.style.background = "#00c1ec";
+      formNotification.style.opacity = "1";
 
       // see doc : https://www.emailjs.com/docs/examples/reactjs/
       // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form, 'YOUR_USER_ID')
         emailjs.sendForm("contact_service", "contact_service", form, USER_ID)
         .then((result) => {
             console.log(result.text);
-            formMessage.innerHTML = "Your message was sent successfully";
+            formNotification.innerHTML = "Your message was sent successfully";
 
-            nameInput.classList.remove("error");
-            emailInput.classList.remove("error");
-            subjectInput.classList.remove("error");
-            messageInput.classList.remove("error");
+            inputs.map(input => input.classList.remove("error"));
             setName("");
             setEmail("");
             setSubject("");
             setMessage("");
 
             setTimeout(() => {
-              formMessage.style.opacity = "0";
+              formNotification.style.opacity = "0";
             }, 5000);
         }, (error) => {
             console.log(error.text);
-            formMessage.style.background = "rgb(253, 87, 87)";
-            formMessage.innerHTML = "An error occured. Please try again!";
+            formNotification.style.background = "rgb(253, 87, 87)";
+            formNotification.innerHTML = "An error occured. Please try again!";
         });
 
     } else {
-      formMessage.innerHTML = "Merci de remplir correctement les champs requis *";
-      formMessage.style.background = "rgb(253, 87, 87)";
-      formMessage.style.opacity = "1";
+      formNotification.innerHTML = "Merci de remplir correctement les champs requis *";
+      formNotification.style.background = "rgb(253, 87, 87)";
+      formNotification.style.opacity = "1";
 
       if (!name) {
         nameInput.classList.add("error");
@@ -103,15 +148,24 @@ const ContactForm = () => {
   return (
     <form className="contact-form">
       <div className="form-content">
-        <input type="text" id="name" name="name" required value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+        <div className="name-content">
+          <label id="name-label">Name field empty</label>
+          <input type="text" id="name" name="name" required value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+        </div>
         <div className="email-content">
-          <label id="not-mail">Email non valide</label>
+          <label id="mail-label">Invalid email</label>
           <input type="mail" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         </div>
-        <input type="text" id="subject" name="subject" required value={subject} onChange={(e) => setSubject(e.target.value)} placeholder="Subject" />
-        <textarea id="message" name="message" required value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" rows="7" />
+        <div className="subject-content">
+          <label id="subject-label">Subject field empty</label>
+          <input type="text" id="subject" name="subject" required value={subject} onChange={(e) => setSubject(e.target.value)} placeholder="Subject" />
+        </div>
+        <div className="message-content">
+          <label id="message-label">Message field empty</label>
+          <textarea id="message" name="message" required value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" rows="7" />
+        </div>
       </div>
-      <div className="formMessage"></div>
+      <div className="form-notification"></div>
       <input className="form-button" type="submit" value="Send" onClick={(e) => handleSubmit(e)} />
     </form>
   );
