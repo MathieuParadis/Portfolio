8e4ee41651af8651cce65e10c92f0aebe084c970
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-18 18:25:54 +0100

    edited portfolio page

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,9 +1,23 @@
+// CONFIG IMPORTS
 import React from 'react';
+
+// COMPONENTS IMPORTS
+
+// COMPONENTS DATA
+import projects from '../data/Projects';
 
 const ProjectModal = () => {
   return (
     <div className="project-modal">
+
       Modal
+      {
+        projects.map((project) => {
+          return (
+            <img src={project.img} key={project.title}/>
+          )
+        })
+      }
     </div>
   );
 };
--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,35 +1,42 @@
+import chalet_and_caviar_screenshot from '../assets/images/projects/chalet_and_caviar.png';
+import follownut_screenshot from '../assets/images/projects/follownut.png';
+import thp_immo_screenshot from '../assets/images/projects/thp_immo.png';
+import feedtruck_screenshot from '../assets/images/projects/feedtruck.png';
+import gossip_project_screenshot from '../assets/images/projects/gossip_project.png';
+
+
 const projects = [
   {
     title: "Chalet and Caviar",
-    img: "../assets/images/projects/chalet_and_caviar.png",
+    img: chalet_and_caviar_screenshot,
     url: "https://live-chaletsandcaviaraspenagency.pantheonsite.io/",
     stack: "Wordpress",
     description: "description"
   },
   {
     title: "FollowNut'", 
-    img: "../assets/images/projects/follownut.png",
+    img: follownut_screenshot,
     url: "https://follownut.herokuapp.com/",
     stack: "React",
     description: "description"
   },
   {
     title: "THP immo'", 
-    img: "../assets/images/projects/thp_immo.png", 
+    img: thp_immo_screenshot,
     url: "https://thp-renting.herokuapp.com/",
     stack: "React",
     description: "description"
   },
   {
     title: "Feedtruck", 
-    img: "../assets/images/projects/feedtruck.png", 
+    img: feedtruck_screenshot,
     url: "https://feedtruck.herokuapp.com/",
     stack: "Rails",
     description: "description"
   },
   {
     title: "The Gossip Project", 
-    img: "../assets/images/projects/gossip_project.png", 
+    img: gossip_project_screenshot,
     url: "https://gossip-project-mat-edition.herokuapp.com/",
     stack: "Rails",
     description: "description"
