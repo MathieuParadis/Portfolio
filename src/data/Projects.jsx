93c00ec5ecbf2bd0412f208150677416fc330f41
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-18 15:53:55 +0100

    added images in assets folder

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,8 +1,32 @@
 const projects = [
   {
-    title: "Tire poject", 
-    img: "", 
-    stack: "react",
+    title: "Chalet and Caviar", 
+    img: "../assets/images/projects/chalet_and_caviar.png", 
+    stack: "Wordpress",
+    description: "description"
+  },
+  {
+    title: "FollowNut'", 
+    img: "../assets/images/projects/follownut.png", 
+    stack: "React",
+    description: "description"
+  },
+  {
+    title: "THP immo'", 
+    img: "../assets/images/projects/thp_immo.png", 
+    stack: "React",
+    description: "description"
+  },
+  {
+    title: "Feedtruck", 
+    img: "../assets/images/projects/feedtruck.png", 
+    stack: "Rails",
+    description: "description"
+  },
+  {
+    title: "The Gossip Project", 
+    img: "../assets/images/projects/gossip_project.png", 
+    stack: "Rails",
     description: "description"
   },
 ];
