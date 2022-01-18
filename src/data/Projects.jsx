541711dd3f64e8bafc98ae56954cf522db096110
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-18 15:58:07 +0100

    updated projects in data folder

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,31 +1,36 @@
 const projects = [
   {
-    title: "Chalet and Caviar", 
-    img: "../assets/images/projects/chalet_and_caviar.png", 
+    title: "Chalet and Caviar",
+    img: "../assets/images/projects/chalet_and_caviar.png",
+    url: "https://live-chaletsandcaviaraspenagency.pantheonsite.io/",
     stack: "Wordpress",
     description: "description"
   },
   {
     title: "FollowNut'", 
-    img: "../assets/images/projects/follownut.png", 
+    img: "../assets/images/projects/follownut.png",
+    url: "https://follownut.herokuapp.com/",
     stack: "React",
     description: "description"
   },
   {
     title: "THP immo'", 
     img: "../assets/images/projects/thp_immo.png", 
+    url: "https://thp-renting.herokuapp.com/",
     stack: "React",
     description: "description"
   },
   {
     title: "Feedtruck", 
     img: "../assets/images/projects/feedtruck.png", 
+    url: "https://feedtruck.herokuapp.com/",
     stack: "Rails",
     description: "description"
   },
   {
     title: "The Gossip Project", 
     img: "../assets/images/projects/gossip_project.png", 
+    url: "https://gossip-project-mat-edition.herokuapp.com/",
     stack: "Rails",
     description: "description"
   },
