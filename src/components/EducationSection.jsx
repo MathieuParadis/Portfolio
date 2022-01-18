2d52cdb7f35623cc4abbd646a417b54687baeb1d
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-18 12:52:50 +0100

    edited education in resume

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -10,22 +10,22 @@ const EducationSection = () => {
       <h3>EDUCATION</h3>
       <div className="content">
         <ExperienceComponent 
-          title="Project Manager" 
-          location="Meiyume" 
-          date="Aug. 2019 - Jul. 2020 " 
-          description="desfdgnfdln gnfdlnhkldfn fdh fd" 
+          title="Bachelor's degree in web development" 
+          location="OpenClassrooms" 
+          date="Nov. 2021 - Present" 
+          description="Use of front-end libraries and frameworks such as Bootstrap, and ReactJS. Building of sites backend in Ruby, Run SQL queries on databases." 
         />
         <ExperienceComponent 
-          title="Quality Engineer" 
-          location="Meiyume" 
-          date="Aug. 2017 - Jul. 2019 " 
-          description="desfdgnfdln gnfdlnhkldfn fdh fd" 
+          title="Web development - Full stack training course" 
+          location="The Hacking Project" 
+          date="Apr. - Oct. 2021" 
+          description="Courses of HTML, CSS, Ruby, Ruby on Rails, JavaScript, and React." 
         />
         <ExperienceComponent 
-          title="Supplier Quality Engineer" 
-          location="Telenco" 
-          date="Nov. 2015 - Jun. 2017 " 
-          description="desfdgnfdln gnfdlnhkldfn fdh fd" 
+          title="Engineering degree in Material Sciences" 
+          location="Phelma, Grenoble Institute of Technology" 
+          date="2012 - 2015" 
+          description="Classes in physics, material properties, and advanced techniques of characterization." 
         />
       </div>
     </div>
