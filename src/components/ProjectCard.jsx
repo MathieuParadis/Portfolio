1cdc0afa4301cae8dbbfd8dbae75113e0fd621f4
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-19 09:34:47 +0100

    edited portfolio section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectCard = (project) => {
   const {title, img} = project
   return (
-    <div className="col-12 col-sm-4 my-3 flip-card">
+    <div className="col-12 col-sm-6 col-lg-4 my-3 p-0 px-sm-2 flip-card">
       <div className="flip-card-inner">
         <div className="flip-card-front">
           <img src={img} alt={title + " illustration"} />
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -13,8 +13,11 @@ const Portfolio = () => {
     <div className="portfolio">
       <h1>Recent projects</h1>
       <div className="projects">
-        Projects here
         <ProjectCard img={projects[0].img} title={projects[0].title}/>
+        <ProjectCard img={projects[1].img} title={projects[1].title}/>
+        <ProjectCard img={projects[2].img} title={projects[2].title}/>
+        <ProjectCard img={projects[3].img} title={projects[3].title}/>
+        <ProjectCard img={projects[4].img} title={projects[4].title}/>
       </div>
       {/* <ProjectModal /> */}
     </div>
