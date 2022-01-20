61f153ab7f07e6cd8ce33ecb78e7ffcba32bc1ae
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-20 06:11:44 +0100

    added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // import pages
@@ -7,7 +7,6 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 
-
 // import components
 import Navigation from './components/Navigation';
 import DarkMode from './components/context/DarkMode';
@@ -15,35 +14,12 @@ import DarkMode from './components/context/DarkMode';
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
-
-  // useEffect(() => {
-  //   const temp = JSON.parse(localStorage.getItem("themePreference"));
-  //   if (temp !== undefined && temp !== null) {
-  //     setDarkMode(temp);
-  //   }
-  //   else if (
-  //     window.matchMedia &&
-  //     window.matchMedia("(prefers-color-scheme: dark)").matches
-  //   ) {
-  //     setDarkMode(true);
-  //   }
-  // });
-
-  // useEffect(() => {
-  //   if (darkMode) {
-  //     const body = document.querySelector("#root");
-  //     body.classList.add("dark-mode");
-  //   }
-  // }, [darkMode]);
-
   const switchMode = () => {
     const body = document.querySelector("body");
     body.classList.toggle("dark-mode");
     // localStorage.setItem("themePreference", String(!darkMode));
     setDarkMode(!darkMode);
   };
-
-
 
   return (
     <div>
