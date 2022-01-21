38cf1becd96aee24598a39881946e204676e3731
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-01-21 15:40:21 +0100

    added project into data

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,11 +1,18 @@
+import movie_festival from '../assets/images/projects/movie_festival.png';
 import chalet_and_caviar_screenshot from '../assets/images/projects/chalet_and_caviar.png';
 import follownut_screenshot from '../assets/images/projects/follownut.png';
 import thp_immo_screenshot from '../assets/images/projects/thp_immo.png';
 import feedtruck_screenshot from '../assets/images/projects/feedtruck.png';
 import gossip_project_screenshot from '../assets/images/projects/gossip_project.png';
 
-
 const projects = [
+  {
+    title: "Watch it Outside - Boston edition",
+    img: movie_festival,
+    url: "https://mathieuparadis.github.io/Boston_movie_festival/",
+    stack: "HTML, CSS, Javascript",
+    description: "Website for the Watch it Outside - Boston edition, an outdoor movie festival taking place in Boston during summer."
+  },
   {
     title: "Chalet and Caviar",
     img: chalet_and_caviar_screenshot,
